c6c9676219cfc7b151069942e6c214d32a909081
chwcode <chwcode@madmanbeta.com>
2017-03-13 02:01:37 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Deploy Script"
 git stash --all
-git remote add fury https://chwcode:$FURY_API_TOKEN@git.fury.io/chwcode/codeship-hello.git
+git remote add fury https://chwcode:$FURY_API_TOKEN@git.fury.io/chwcode/travis-hello.git
 git push fury master
